98403627266ba6edd83f27f4f083962d2773db36
waqas9291 <58703225+waqas9291@users.noreply.github.com>
2025-12-16 14:27:38 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // This must match your GitHub repository name exactly
+  // This must match your GitHub repository name exactly:
+  // If your link is https://waqas9291.github.io/Trade-Journal/ then this must be '/Trade-Journal/'
   base: '/Trade-Journal/',
 })
