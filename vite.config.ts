ea3e3d543e30f81744d403d74a61cfe08268efff
waqas9291 <58703225+waqas9291@users.noreply.github.com>
2025-12-16 14:47:06 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // Using './' makes the app work on any repository name (Trade-Journal or Trade-Journal-2.0)
   base: './',
 })
