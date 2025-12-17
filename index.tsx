7df2530c7386f652041ae33231222f13d8be97c2
waqas9291 <58703225+waqas9291@users.noreply.github.com>
2025-12-17 17:54:37 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './App.tsx';
 import './index.css';
 
 const rootElement = document.getElementById('root');
@@ -15,3 +15,4 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
